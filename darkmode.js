88a66be1bdae00758655a7515f00c44febb3dbe9
McDony Lee <mcdonyleee@gmail.com>
2023-03-31 21:39:09 +1100

    Dark mode mockup ver 0.2 with Dark mode toggle bug

---
+++ darkmode.js
@@ -0,0 +1,67 @@
+const html = document.querySelector("html");
+const toggleEl = document.querySelector(".toggle");
+const table = document.querySelector(".table");
+
+// Check with localStorage which theme to apply
+function setTheme() {
+    if (localStorage.getItem("theme") === "light") {
+        changeThemeTo("light");
+    } else {
+        changeThemeTo("dark");
+    }
+}
+
+// Change theme with it's name in params
+function changeThemeTo(theme = "default") {
+    if (theme === "dark") {
+        localStorage.setItem("theme", "dark");
+        html.classList.add("dark");
+
+        table.classList.add("table-dark");
+
+        toggleEl.innerHTML = "Light mode";
+        toggleEl.classList.add("btn-dark");
+    } else {
+        localStorage.setItem("theme", "light");
+
+        html.classList.add("table-light");
+        html.classList.remove("dark");
+
+        toggleEl.innerHTML = "Dark mode";
+        toggleEl.classList.add("btn-light");
+    }
+}
+
+// change the light to dark here
+toggleEl.addEventListener("click", (e) => {
+    //change to light
+    if (html.classList.contains("dark")) {
+        html.classList.remove("dark");
+
+        table.classList.add("table-light");
+        table.classList.remove("table-dark");
+        e.target.innerHTML = "Dark mode";
+
+        toggleEl.classList.remove("btn-dark");
+        toggleEl.classList.add("btn-light");
+    }
+    //change to dark
+    else {
+        html.classList.add("dark");
+
+        table.classList.remove("table-light");
+        table.classList.add("table-dark");
+        e.target.innerHTML = "light mode";
+
+        toggleEl.classList.remove("btn-light");
+        toggleEl.classList.add("btn-dark");
+    }
+    // html.classList.toggle("dark")
+    if (localStorage.getItem("theme") === "light") {
+        changeThemeTo("dark");
+    } else {
+        changeThemeTo("light");
+    }
+});
+
+setTheme();
