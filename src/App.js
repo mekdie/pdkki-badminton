6e79985b33e9882d29a5c686448cf9f1703600f7
McDony Lee <mcdonyleee@gmail.com>
2023-04-02 17:06:55 +1000

    Added basic access to firebase db

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
-
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase-config";
 function App() {
+    const [users, setUsers] = useState([]);
+
+    const usersCollectionRef = collection(db, "users");
+
+    useEffect(() => {
+        // reference:
+        // https://firebase.google.com/docs/firestore/query-data/get-data
+        const getUsers = async () => {
+            const data = await getDocs(usersCollectionRef);
+            // console.log(data.docs[0].data());
+            setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        };
+
+        getUsers();
+    }, []);
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to from outside.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            {" "}
+            {users.map((user) => {
+                return (
+                    <div>
+                        <h1>Name: {user.name}</h1>
+                    </div>
+                );
+            })}
         </div>
     );
 }
